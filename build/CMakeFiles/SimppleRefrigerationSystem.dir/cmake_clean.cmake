6c48b1c64f0e0116446000218134ca98857b3a6a
Casper Bladt <casper.bladt@bitzerdk.com>
2021-10-13 13:19:46 +0200

    initial

---
+++ build/CMakeFiles/SimppleRefrigerationSystem.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimppleRefrigerationSystem.dir/main.cpp.o"
+  "SimppleRefrigerationSystem"
+  "SimppleRefrigerationSystem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SimppleRefrigerationSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "simpp/IOBlock.hpp"
+#include "simpp/Component/Orifice.hpp"
+
+int main()
+{
+  simpp::IOBlock<simpp::Component::Orifice<float>> orifice;
+}
